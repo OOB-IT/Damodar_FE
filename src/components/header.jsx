e5ade571a3fe781a87658867d53975718e55323d
harsh <harshchauhan291@gmail.com>
2024-06-25 00:11:15 +0530

    video added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import bgVideo from "../asset/video.mp4";
 
 export const Header = (props) => {
   return (
-    <header id='header'>
-      <div className='intro'>
-        <div className='overlay'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-8 col-md-offset-2 intro-text text-center'>
+    <header id="header">
+      <div className="intro">
+        <video autoPlay muted loop id="background-video">
+          <source src={bgVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text text-center">
                 <h1 style={{ fontSize: "3rem" }}>
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
