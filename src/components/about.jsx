22f5ac0e45958a5b03c7124443647e7c7ee701ba
harsh <harshchauhan291@gmail.com>
2024-07-27 21:38:15 +0530

    about and review

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,10 +6,35 @@ import { Features } from "./features";
 
 export const About = (props) => {
   const [landingPageData, setLandingPageData] = useState({});
+  const [sectionDetails, setSectionDetails] = useState(null);
+  const fetchSectionDetails = async () => {
+    try {
+      const response = await fetch(
+        "https://api.damodarr.com/api/getSectionDetails/1"
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log("API Response:", data.data);
+        setSectionDetails(data?.data);
+      } else {
+        console.error("Error fetching section details:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching section details:", error);
+    }
+  };
   useEffect(() => {
     setLandingPageData(JsonData);
+    fetchSectionDetails();
   }, []);
   const fdata = landingPageData.Features;
+
+  // Convert \r\n\r\n to <br /><br />
+  const formatSectionDesc = (text) => {
+    console.log("////", text);
+    if (!text) return "";
+    return text.replace(/\n/g, "<br />");
+  };
 
   return (
     <div id="about">
@@ -27,9 +52,13 @@ export const About = (props) => {
           <div className="col-xs-12 col-md-6">
             <div className=" ">
               <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph1 : "loading..."}</p>
-              <p>{props.data ? props.data.paragraph2 : "loading..."}</p>
-              <p>{props.data ? props.data.paragraph3 : "loading..."}</p>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: props.data
+                    ? formatSectionDesc(sectionDetails?.sectionDesc)
+                    : "laoding....",
+                }}
+              ></p>
               <button type="submit" className="btn btn-custom btn-lg rounded">
                 <Link style={{ color: "#f5f5f5" }} to="/company">
                   View More
